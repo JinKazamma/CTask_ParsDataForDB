57b516aa1501d6fe4119dcf7d1068448862c264c
JinKazamma <gerttyyy@gmail.com>
2023-05-31 14:41:45 +0500

    Entered in Mysql

--- main.c
+++ main.c
@@ -14,6 +14,26 @@ int main()
     const char* strForHash = "hello world";
     unsigned char hash[SHA256_DIGEST_LENGTH];
     hashing(strForHash,strlen(strForHash),hash);
-    
+
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
+    const char *server = "localhost";
+    const char *user = "Testuser";
+    const char *password = "13243546Aa@";
+    const char* database = "Base4Test";
+
+    conn = mysql_init(NULL);
+    if(!mysql_real_connect(conn,server,user,password,database,0,NULL,0))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
+    else 
+    printf("Entered in %s\n",database);
+
+
     return 0;
 }
