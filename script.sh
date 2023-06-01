3bb808b242b4577f6f0e366044e4c1bf9339f842
JinKazamma <gerttyyy@gmail.com>
2023-06-01 17:39:44 +0500

    final buld

--- script.sh
+++ script.sh
@@ -11,9 +11,9 @@ sudo apt-get update
 sudo apt-get install -y mysql-server libmysqlclient-dev
 
 # Создание пользователя
-mysql -uroot -p -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASS'"
-mysql -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost'"
+mysql -uroot -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost'"
 
 # Создание базы данных и таблицы
-mysql -uroot -p -e "CREATE DATABASE $MYSQL_DATABASE"
-mysql -uroot -p -e "USE $MYSQL_DATABASE; CREATE TABLE $MYSQL_TABLE (string VARCHAR(255), hash VARCHAR(255))"
+mysql -u$MYSQL_USER -p$MYSQL_USER_PASS -e "CREATE DATABASE $MYSQL_DATABASE"
+mysql -u$MYSQL_USER -p$MYSQL_USER_PASS -e "USE $MYSQL_DATABASE; CREATE TABLE $MYSQL_TABLE (string VARCHAR(255), hash VARCHAR(255))"
